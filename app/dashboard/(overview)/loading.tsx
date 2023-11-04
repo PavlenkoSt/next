79a756fffd37599f5b3c49cab21c96e7ab146d5c
Stanislav <verbatim8872@gmail.com>
2023-11-05 00:02:23 +0200

    invoice forms

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardSkeleton from "@/app/ui/skeletons";
+import React from "react";
+
+export default function loading() {
+  return <DashboardSkeleton />;
+}
---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,37 @@
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { InvoiceForm } from "@/app/lib/definitions";
+import { notFound } from "next/navigation";
+
+interface IProps {
+  params: {
+    id: string;
+  };
+}
+
+export default async function Page({ params }: IProps) {
+  const id = params.id;
+  const [customers, invoice] = await Promise.all([
+    fetchCustomers(),
+    fetchInvoiceById(id),
+  ]);
+
+  if (!invoice) return notFound();
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice as InvoiceForm} customers={customers} />
+    </main>
+  );
+}
