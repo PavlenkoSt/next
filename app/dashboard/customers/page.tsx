f7906baa59fb83ecd1d4251766410723be387808
Stanislav <verbatim8872@gmail.com>
2023-11-05 12:05:46 +0200

    metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,21 @@
+import { Metadata } from "next";
 import React from "react";
+import CustomersTable from "@/app/ui/customers/table";
+import { fetchFilteredCustomers } from "@/app/lib/data";
 
-export default function Customers() {
-  return <div>Customers</div>;
+interface IProps {
+  searchParams?: {
+    query?: string;
+  };
+}
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
+
+export default async function Customers({ searchParams }: IProps) {
+  const query = searchParams?.query || "";
+  const filteredCustomers = await fetchFilteredCustomers(query);
+
+  return <CustomersTable customers={filteredCustomers} />;
 }
--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,12 +3,17 @@ import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
 import { InvoiceForm } from "@/app/lib/definitions";
 import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
 interface IProps {
   params: {
     id: string;
   };
 }
+
+export const metadata: Metadata = {
+  title: "Edit invoice",
+};
 
 export default async function Page({ params }: IProps) {
   const id = params.id;
